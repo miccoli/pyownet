a4ebaed274fea34aa1f3c24c033e1bd71c73cf3f
miccoli <miccoli@d13e4a39-8c2f-45fd-874f-7e6b0362a1bc>
2011-09-23 09:09:05 +0000

    first working draft added CGI script and corresponding client to query thermal state

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ __version__ = '$Revision$'
 from distutils.core import setup
 
 setup(name = 'pyownet',
-      version = '0.0',
-      description = 'python ownet client',
+      version = '0.1',
+      description = 'python ownet client library',
       author = 'Stefano Miccoli',
       author_email = 'stefano.miccoli@polimi.it',
       package_dir = {'': 'lib', },
       py_modules = ['pyownet.protocol', ],
-      #scripts = [ 'scripts/purgereg.py', 'scripts/syncsub.py', ],
+      scripts = [ 'scripts/status.py', 'scripts/digistate.py', ],
      )
