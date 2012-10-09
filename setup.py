f00bdcbe439e4ee8401f3b641810aa2dd3cd6668
miccoli <miccoli@d13e4a39-8c2f-45fd-874f-7e6b0362a1bc>
2012-10-09 19:41:34 +0000

    cleanup

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ __version__ = '$Revision$'
 from distutils.core import setup
 
 setup(name = 'pyownet',
-      version = '0.2',
+      version = '0.3',
       description = 'python ownet client library',
       author = 'Stefano Miccoli',
       author_email = 'stefano.miccoli@polimi.it',
       package_dir = {'': 'lib', },
-      py_modules = ['pyownet.protocol', ],
+      packages = ['pyownet', ],
       scripts = [ 'scripts/status.py', 'scripts/digistate.py', ],
      )
