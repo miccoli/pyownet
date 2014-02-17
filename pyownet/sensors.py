da2e31e01b0c8bf9834a4578ae03502fceced91f
Stefano Miccoli <stefano.miccoli@polimi.it>
2014-02-18 00:05:10 +0100

    define only 'present' sensor attributes

--- pyownet/sensors.py
+++ pyownet/sensors.py
@@ -1,9 +1,27 @@
 # sensors
 
+#
+# Copyright 2013, 2014 Stefano Miccoli
+# 
+# This python package is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 from __future__ import print_function
 
 import posixpath
 import types
+import re
 
 from pyownet import protocol
 
@@ -52,24 +70,28 @@ def _pathname_mangle(path):
     path = path.replace('.ALL', '')
     return path
 
-def _parse_structure(proxy, family):
+def _walk(proxy, root):
 
-    def walk(path):
-        namespace = dict()
-        pre = len(path)
-        for i in proxy.dir(path):
-            if i.endswith('/'):
-                namespace[i[pre:-1]] = walk(i)
-            else:
-                namespace[i[pre:]] = Properties(proxy.read(i))
-                #print(i.ljust(35), Properties(proxy.read(i)))
-        return namespace
+    assert root.endswith('/')
+    ents = list(proxy.dir(root, slash=True))
+    while ents:
+        ent = ents.pop()
+        if ent.endswith('/'):
+            ents.extend(proxy.dir(ent))
+        else:
+            yield ent
 
-    path = '/structure/%s/' % family
-    assert proxy.present(path)
-    return walk(path)
+def _fetch_structure(proxy, family):
+
+    stru = dict()
+    for i in _walk(proxy, '/structure/{0}/'.format(family)):
+        name = i.split('/',3)[-1]
+        stru[name] = Properties(proxy.read(i))
+    return stru
 
 def _sens_namespace(proxy, entity):
+
+    rex = re.compile(r'.+\.[0-9]+')
 
     def getter(path, name, val):
         cast = _TYPE_CODE[val.type]
@@ -79,30 +101,33 @@ def _sens_namespace(proxy, entity):
         read.__name__ = str(name)
         return read
 
-    def walk(path,stru):
+    def walk(path):
         namespace = dict()
-        for k, val in stru.iteritems():
-            pk = posixpath.join(path, k)
-            mk = _pathname_mangle(k)
-            if isinstance(val, dict):
-                namespace[mk] = walk(pk, val)
+        pre = len(path)
+        for i in proxy.dir(path):
+            if i.endswith('/'):
+                name = _pathname_mangle(i[pre:-1])
+                namespace[name] = walk(i)
             else:
+                name = _pathname_mangle(i[pre:])
+                base = i.split('/', 2)[-1]
+                if rex.match(base):
+                    continue
+                val = stru[base]
                 assert isinstance(val, Properties)
                 if val.mode not in ('ro', 'rw'):
                     continue
-                if k.endswith('.0'):
-                    continue
                 if val.pers == 'f':
-                    namespace[mk] = getter(pk, mk, val)()
+                    namespace[name] = getter(i, name, val)()
                 else:
-                    namespace[mk] = staticmethod(getter(pk, mk, val))
+                    namespace[name] = staticmethod(getter(i, name, val))
         return namespace
 
     assert proxy.present(entity)
     assert proxy.present(posixpath.join(entity, 'family'))
     fam = proxy.read(posixpath.join(entity, 'family')).decode()
-    stru = _parse_structure(proxy, fam)
-    namespace = walk(entity, stru) 
+    stru = _fetch_structure(proxy, fam)
+    namespace = walk(entity) 
     return namespace
 
 def getsensor(proxy, path):
@@ -110,25 +135,26 @@ def getsensor(proxy, path):
     ns = _sens_namespace(proxy, path)
     return metasensor(ns['type'], (_sensor, object), ns)()
 
-def _test():
+def _main():
     def walk(prefix, s):
         for att in dir(s):
             fatt = getattr(s, att, None)
             if isinstance(fatt, types.FunctionType):
                 try:
-                    print(prefix, att.ljust(12), fatt())
+                    print(prefix, '{0}()'.format(att).ljust(13), fatt())
                 except protocol.OwnetError as exp:
-                    print(prefix, att.ljust(12), exp)
+                    print(prefix, '{0}()'.format(att).ljust(13), exp)
             elif isinstance(fatt, str) and not fatt.startswith('__'):
-                print(prefix, att.ljust(12), fatt)
+                print(prefix, att.ljust(13), fatt)
             elif isinstance(fatt, _sensor):
-                print(prefix, att.ljust(12))
+                print(prefix, '{0}/'.format(att).ljust(13))
                 walk('    ' + prefix, fatt)
     proxy = protocol.OwnetProxy()
     for i in proxy.dir():
         s = getsensor(proxy, i)
         print(s)
         walk('|--', s)
+        print()
 
 if __name__ == '__main__':
-    _test()
+    _main()
